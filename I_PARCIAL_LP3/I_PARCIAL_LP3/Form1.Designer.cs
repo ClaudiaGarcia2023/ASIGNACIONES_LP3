b5f0870d732f8b25b62f90ed04420e1b2a958d0f
ClaudiaGarcia2023 <123711432+ClaudiaGarcia2023@users.noreply.github.com>
2023-02-04 00:53:27 -0600

    EJERCICIO 1
    Desarrollo de la tarea 1 , ejercicio 1
    
    Saludos.

---
+++ I_PARCIAL_LP3/I_PARCIAL_LP3/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace I_PARCIAL_LP3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txb1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Favor ingresar un numero";
+            // 
+            // txb1
+            // 
+            this.txb1.Location = new System.Drawing.Point(291, 27);
+            this.txb1.Name = "txb1";
+            this.txb1.Size = new System.Drawing.Size(98, 26);
+            this.txb1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(249, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Tipo_numero";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(498, 244);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txb1);
+            this.Controls.Add(this.label2);
+            this.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txb1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ I_PARCIAL_LP3/I_PARCIAL_LP3/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace I_PARCIAL_LP3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+            private void button1_Click(object sender, EventArgs e)
+
+            {
+                int Num = Convert.ToInt16(txb1.Text);
+
+                if (Num % 2 == 0 & Num >= 0)
+                {
+                    MessageBox.Show("Numero " + Num + " ingresado es par y positivo");
+
+                }
+                else if (Num % 2 == 0 & Num <= 0)
+                {
+                    MessageBox.Show("Numero " + Num + " ingresado es par y negativo");
+
+                }
+                else if (Num % 2 != 0 & Num >= 0)
+                {
+                    MessageBox.Show("Numero " + Num + " ingresado es impar y positivo");
+
+                }
+                else
+                    MessageBox.Show("Numero " + Num + " ingresado es impar y negativo");
+
+            }
+        }
+    }
